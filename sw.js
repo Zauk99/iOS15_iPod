2bec70bf1bde1c2a6902a27ac135f19996706c5b
Zauk99 <pablohd140605@gmail.com>
2026-03-22 21:53:54 +0100

    version 1

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'ipod-v4';
+const BASE = '/iPod_iOS15';
+const ASSETS = [
+  BASE + '/',
+  BASE + '/index.html',
+  BASE + '/icon.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(res => {
+        if (res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => null);
+      return cached || net || new Response('Offline', { status: 503 });
+    })
+  );
+});
